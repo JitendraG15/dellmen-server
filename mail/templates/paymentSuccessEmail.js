1fe426673519111899f7d8f1f8d865cf0d331494
Jitendra Gupta <jitendragupta@Jitendras-MacBook-Air.local>
2024-03-12 13:11:47 +0530

    Updated

--- mail/templates/paymentSuccessEmail.js
+++ mail/templates/paymentSuccessEmail.js
@@ -67,8 +67,8 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
-                    alt="StudyNotion Logo"></a>
+            <a href="https://www.dellmen.com"><img class="logo" src="https://res.cloudinary.com/dwyzbgbzh/image/upload/v1710219549/DELLMEN/dellmen-removebg-preview_yy0mpk.png"
+                    alt="Dellmen Logo"></a>
             <div class="message">Course Payment Confirmation</div>
             <div class="body">
                 <p>Dear ${name},</p>
@@ -77,7 +77,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
                 <p>Your Order ID is <b>${orderId}</b></p>
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                    href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                    href="mailto:info@dellmen.com">info@dellmen.com</a>. We are here to help you!</div>
         </div>
     </body>
     
--- utils/mailSender.js
+++ utils/mailSender.js
@@ -12,7 +12,7 @@ const mailSender = async (email, title, body) => {
     })
 
     let info = await transporter.sendMail({
-      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`, // sender address
+      from: `"Dellmen | An Ed-Tech Platform" <${process.env.MAIL_USER}>`, // sender address
       to: `${email}`, // list of receivers
       subject: `${title}`, // Subject line
       html: `${body}`, // html body
